7ebe8a79f580262b6e97d6310f264ffe698827f1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-05-31 16:27:30 +1000

    setup code stub for xp xeff joint distributions

---
+++ tests/test_xp_eff_joint_prior.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import unittest
+
+from effective_spins import joint_xp_xeff_prior
+from . import utils
+
+
+class TestXpXeffJointPrior(unittest.TestCase):
+    def setUp(self):
+        self.outdir = "tests/xeff_testoutdir"
+        os.makedirs(self.outdir, exist_ok=True)
+
+    def tearDown(self):
+        if os.path.exists(self.outdir):
+            shutil.rmtree(self.outdir)
+
+    def test_prior_on_xeff(self):
+        val = joint_xp_xeff_prior.p_xp_xeff(xp=0.1, xeff=1)
+        self.assertNotEqual(1, val)
+
+
+if __name__ == "__main__":
+    unittest.main()
